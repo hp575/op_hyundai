f4035476b6e8585263e720aec59701c3b7aebe92
sangwoo <33356618+gomtings@users.noreply.github.com>
2022-11-30 14:03:24 +0900

    fix

--- panda/board/safety/safety_hyundai_common.h
+++ panda/board/safety/safety_hyundai_common.h
@@ -42,7 +42,7 @@ void hyundai_common_cruise_state_check(const int cruise_engaged) {
   // so keep track of user button presses to deny engagement if no interaction
 
   // enter controls on rising edge of ACC and recent user button press, exit controls when ACC off
-  if (hyundai_longitudinal) { // 1 || ! 롱컨트롤이 아닐경우에 만 동작? 
+  if (!hyundai_longitudinal) { // 1 || ! 롱컨트롤이 아닐경우에 만 동작? 
     if (cruise_engaged && !cruise_engaged_prev && (hyundai_last_button_interaction < HYUNDAI_PREV_BUTTON_SAMPLES)) {
       controls_allowed = 1;
     }
@@ -62,7 +62,7 @@ void hyundai_common_cruise_buttons_check(const int cruise_button, const int main
     hyundai_last_button_interaction = MIN(hyundai_last_button_interaction + 1U, HYUNDAI_PREV_BUTTON_SAMPLES);
   }
 
-  if (!hyundai_longitudinal) { // 롱컨트롤 일때 동작함..
+  if (hyundai_longitudinal) { // 롱컨트롤 일때 동작함..
     // enter controls on falling edge of resume or set
     bool set = (cruise_button != HYUNDAI_BTN_SET) && (cruise_button_prev == HYUNDAI_BTN_SET);
     bool res = (cruise_button != HYUNDAI_BTN_RESUME) && (cruise_button_prev == HYUNDAI_BTN_RESUME);

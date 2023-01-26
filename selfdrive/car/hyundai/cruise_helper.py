a07cb1ed1146defa6353f8ec429fd82f6d9da9b3
LEESNAGWOO <33356618+gomtings@users.noreply.github.com>
2023-01-26 09:42:38 +0900

    enable radar tracks update  - del fw_ver
    - del fw_ver

--- selfdrive/car/hyundai/cruise_helper.py
+++ selfdrive/car/hyundai/cruise_helper.py
@@ -12,6 +12,39 @@ from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
 # ajouatom
 def enable_radar_tracks(CP, logcan, sendcan):
       # START: Try to enable radar tracks
+  print("Try to enable radar tracks")
+  if CP.openpilotLongitudinalControl and CP.carFingerprint in [CAR.NEXO]:
+    rdr_fw = None
+    rdr_fw_address = 0x7d0 #일부차량은 다름..
+    if True:
+      for i in range(10):
+        print("O yes")
+      try:
+        for i in range(40):
+          try:
+            query = IsoTpParallelQuery(sendcan, logcan, CP.sccBus, [rdr_fw_address], [b'\x10\x07'], [b'\x50\x07'], debug=True)
+            for addr, dat in query.get_data(0.1).items(): # pylint: disable=unused-variable
+              print("ecu write data by id ...")
+              new_config = b"\x00\x00\x00\x01\x00\x01"
+              #new_config = b"\x00\x00\x00\x00\x00\x01"
+              dataId = b'\x01\x42'
+              WRITE_DAT_REQUEST = b'\x2e'
+              WRITE_DAT_RESPONSE = b'\x68'
+              query = IsoTpParallelQuery(sendcan, logcan, CP.sccBus, [rdr_fw_address], [WRITE_DAT_REQUEST+dataId+new_config], [WRITE_DAT_RESPONSE], debug=True)
+              query.get_data(0)
+              print(f"Try {i+1}")
+              break
+            break
+          except Exception as e:
+            print(f"Failed {i}: {e}") 
+      except Exception as e:
+        print("Failed to enable tracks" + str(e))
+  print("END Try to enable radar tracks")
+  # END try to enable radar tracks
+  
+"""
+def enable_radar_tracks(CP, logcan, sendcan):
+      # START: Try to enable radar tracks
   print("Try to enable radar tracks")  
   # if self.CP.openpilotLongitudinalControl and self.CP.carFingerprint in [HYUNDAI_CAR.SANTA_FE_2022]:
   if CP.openpilotLongitudinalControl and CP.carFingerprint in [CAR.SANTA_FE_HEV_2022, CAR.NEXO]:
@@ -52,3 +85,4 @@ def enable_radar_tracks(CP, logcan, sendcan):
         print("Failed to enable tracks" + str(e))
   print("END Try to enable radar tracks")
   # END try to enable radar tracks
+"""

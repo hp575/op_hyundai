c9e8ddbcc76cc74bba0e9fd78f6772678be36c2f
leesangwoo <tkddn4508@gmail.com>
2023-02-27 23:48:48 +0900

    merge changes 0227
    cleanup cruise state manager - d9c87fd
    merge changes - 52a4378
    control gapAdjust - a155598
    show correct cluster leads - fe1b80f
    refactor - fdb8209

--- selfdrive/controls/neokii/cruise_state_manager.py
+++ selfdrive/controls/neokii/cruise_state_manager.py
@@ -94,12 +94,13 @@ class CruiseStateManager:
     self.prev_brake_pressed = CS.brakePressed
 
     CS.cruiseState.available = self.available
+    CS.cruiseState.gapAdjust = self.gapAdjust
 
     if cruise_state_control:
       CS.cruiseState.enabled = False
       CS.cruiseState.standstill = False
       CS.cruiseState.speed = self.speed
-      CS.cruiseState.gapAdjust = self.gapAdjust
+      #CS.cruiseState.gapAdjust = self.gapAdjust
       
     if self.enabled : # 롱컨 시작
       CS.cruiseState.enabled = self.enabled

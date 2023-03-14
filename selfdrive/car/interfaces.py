757ed4ec0c89f832f7d4eef91621c67244b3902e
LEESNAGWOO <33356618+gomtings@users.noreply.github.com>
2023-03-14 10:12:41 +0900

    fix

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -306,7 +306,7 @@ class CarInterfaceBase(ABC):
     # we engage when pcm is active (rising edge)
     # enabling can optionally be blocked by the car interface
     if pcm_enable:
-      if cs_out.cruiseState.available and not self.CS.out.cruiseState.available and allow_enable:
+      if cs_out.cruiseState.available and not self.CS.out.cruiseState.available or allow_enable:
         events.add(EventName.pcmEnable)
       elif not cs_out.cruiseState.available:
         events.add(EventName.pcmDisable)
